829ac23f2568e0f30a51326bad5030f5d7fbeba4
Angel Gallego <angelgb1990@gmail.com>
2024-11-11 19:07:02 +0000

    arreglos responsive

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -10,101 +10,107 @@ const Home = () => {
 
 	return (
 		<div>
-			<div className="cabecero"><h1 className="text-center p-5">Todo List</h1></div>
-			<div className=" hoja hoja3 p-4">
-				<div className="container input-group flex-nowrap p-3 ps-5">
-					<span className="input-group-text" id="addon-wrapping">Introduce tareas</span>
-					<input type="text" className="form-control" placeholder="Ejemplo programar 35 horas al dia"
-						onChange={(e) => {
-							setImputValue(e.target.value);
-						}}
-						value={imputValue}
-						onKeyUp={(e) => {
-							if (e.key === "Enter" && imputValue !== "") {
-								setTodoList([...todoList, imputValue])
-								setImputValue("")
-							}
-						}}
-					/>
-				</div>
-				<div>
-					<div className="ps-3 container">
-						<ul><h3 className="p-3 text-center">Tareas pendientes...</h3>
-							{
-								todoList.length === 0 ? (
-									<p className="text-center">¡No quedan tareas pendientes! <img src="https://media.tenor.com/12vvBX1HaPUAAAAi/dance.gif" alt="" /> </p>
-								) :
-									todoList.map((item, index) => {
-										return (
-												<h5 key={index} className="tareapendiente">
-												<input class="form-check-input" type="checkbox" />
-												- {item}
-												<FontAwesomeIcon onClick={() => {
-													setTodoList(todoList.filter((e, i) => i !== index))
-												}}
-													className="eliminar float-end"
-													icon={faTrash} />
-											</h5>	
-										)
-									})}
-						</ul>
+			<div className="cabecero">
+				<h1 className="text-center p-5">Todo List</h1>
+			</div>
+			<div className="d-flex justify-content-center">
+				<div className=" hoja hoja3 p-4 col-11 col-sm-9 col-md-7 col-lg-4">
+					<div className="container input-group flex-nowrap ps-5">
+						<span className="input-group-text" id="addon-wrapping">Introduce tareas</span>
+						<input type="text" className="form-control" placeholder="Ejemplo programar 35 horas al dia"
+							onChange={(e) => {
+								setImputValue(e.target.value);
+							}}
+							value={imputValue}
+							onKeyUp={(e) => {
+								if (e.key === "Enter" && imputValue !== "") {
+									setTodoList([...todoList, imputValue])
+									setImputValue("")
+								}
+							}}
+						/>
 					</div>
-					<div className="container text-end">
-						<p>{todoList.length} tareas restantes </p>
+					<div>
+						<div className="ps-3 container">
+							<ul><h3 className="p-3 text-center">Tareas pendientes...</h3>
+								{
+									todoList.length === 0 ? (
+										<p className="text-center">¡No quedan tareas pendientes! <img src="https://media.tenor.com/12vvBX1HaPUAAAAi/dance.gif" alt="" /> </p>
+									) :
+										todoList.map((item, index) => {
+											return (
+												<h5 key={index} className="tareapendiente flex-nowrap">
+													<input class="form-check-input" type="checkbox" />
+													- {item}
+													<FontAwesomeIcon onClick={() => {
+														setTodoList(todoList.filter((e, i) => i !== index))
+													}}
+														className="eliminar float-end"
+														icon={faTrash} />
+												</h5>
+											)
+										})}
+							</ul>
+						</div>
+						<div className="container text-end">
+							<p>{todoList.length} tareas restantes </p>
+						</div>
 					</div>
-				</div>
 
+				</div>
+				<div className="hoja hoja2 p-4 col-11 col-sm-9 col-md-7 col-lg-4">
+					<div className="container input-group flex-nowrap p-3 ps-5">
+						<span className="input-group-text" id="addon-wrapping">Introduce tareas</span>
+						<input type="text" className="form-control" placeholder="Ejemplo programar 35 horas al dia" />
+					</div>
+					<div>
+						<div className="ps-3 container">
+							<ul><h3 className="p-3 text-center">Tareas pendientes...</h3>
+								{
+									todoList.length === 0 ? (
+										<p className="text-center">¡No quedan tareas pendientes! <img src="https://media.tenor.com/12vvBX1HaPUAAAAi/dance.gif" alt="" /> </p>
+									) :
+										todoList.map((item, index) => {
+											return (
+												<h5 key={index}><FontAwesomeIcon
+													icon={faTrash} /> - {item}  </h5>
+											)
+										})}h5
+							</ul>
+						</div>
+						<div className="container text-end">
+							<p>tareas restantes </p>
+						</div>
+					</div>
+				</div>
+				<div className="hoja hoja1 p-4 col-11 col-sm-9 col-md-7 col-lg-4">
+					<div className="container input-group flex-nowrap p-3 ps-5">
+						<span className="input-group-text" id="addon-wrapping">Introduce tareas</span>
+						<input type="text" className="form-control" placeholder="Ejemplo programar 35 horas al dia" />
+					</div>
+					<div>
+						<div className="ps-3 container">
+							<ul><h3 className="p-3 text-center">Tareas pendientes...</h3>
+								{
+									todoList.length === 0 ? (
+										<p className="text-center">¡No quedan tareas pendientes! <img src="https://media.tenor.com/12vvBX1HaPUAAAAi/dance.gif" alt="" /> </p>
+									) :
+										todoList.map((item, index) => {
+											return (
+												<h5 key={index}><FontAwesomeIcon
+													icon={faTrash} /> - {item}  </h5>
+											)
+										})}
+							</ul>
+						</div>
+						<div className="container text-end">
+							<p>tareas restantes </p>
+						</div>
+					</div>
+				</div>
 			</div>
-			<div className="hoja hoja2 p-4">
-				<div className="container input-group flex-nowrap p-3 ps-5">
-					<span className="input-group-text" id="addon-wrapping">Introduce tareas</span>
-					<input type="text" className="form-control" placeholder="Ejemplo programar 35 horas al dia" />
-				</div>
-				<div>
-					<div className="ps-3 container">
-						<ul><h3 className="p-3 text-center">Tareas pendientes...</h3>
-							{
-								todoList.length === 0 ? (
-									<p className="text-center">¡No quedan tareas pendientes! <img src="https://media.tenor.com/12vvBX1HaPUAAAAi/dance.gif" alt="" /> </p>
-								) :
-									todoList.map((item, index) => {
-										return (
-											<h5 key={index}><FontAwesomeIcon
-												icon={faTrash} /> - {item}  </h5>
-										)
-									})}h5
-						</ul>
-					</div>
-					<div className="container text-end">
-						<p>tareas restantes </p>
-					</div>
-				</div>
-			</div>
-			<div className="hoja hoja1 p-4">
-				<div className="container input-group flex-nowrap p-3 ps-5">
-					<span className="input-group-text" id="addon-wrapping">Introduce tareas</span>
-					<input type="text" className="form-control" placeholder="Ejemplo programar 35 horas al dia" />
-				</div>
-				<div>
-					<div className="ps-3 container">
-						<ul><h3 className="p-3 text-center">Tareas pendientes...</h3>
-							{
-								todoList.length === 0 ? (
-									<p className="text-center">¡No quedan tareas pendientes! <img src="https://media.tenor.com/12vvBX1HaPUAAAAi/dance.gif" alt="" /> </p>
-								) :
-									todoList.map((item, index) => {
-										return (
-											<h5 key={index}><FontAwesomeIcon
-												icon={faTrash} /> - {item}  </h5>
-										)
-									})}
-						</ul>
-					</div>
-					<div className="container text-end">
-						<p>tareas restantes </p>
-					</div>
-				</div>
-			</div>
+
+
 		</div>
 	);
 };
